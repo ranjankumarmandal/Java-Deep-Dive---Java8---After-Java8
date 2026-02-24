ac662f45c6328897e0688b290fdc47c773c7aa15
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-02-24 22:00:39 +0530

    Advanced Java: intial setup - bugfix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Advanced Java")
+        System.out.println("Advanced Java");
     }
 }
