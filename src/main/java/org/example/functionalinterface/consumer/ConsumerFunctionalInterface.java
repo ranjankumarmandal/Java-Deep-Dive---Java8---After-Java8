d6a2c242fb25e4f1054d045be48706842486c16a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-02-25 21:55:11 +0530

    Advanced Java: completed implementation of  Consumer Functional Interface

--- src/main/java/org/example/functionalinterface/consumer/ConsumerFunctionalInterface.java
+++ src/main/java/org/example/functionalinterface/consumer/ConsumerFunctionalInterface.java
@@ -5,5 +5,7 @@ import java.util.function.Consumer;
 public class ConsumerFunctionalInterface {
     public static void main(String[] args) {
         Consumer<String> c = (name) -> System.out.println(name);
+
+        c.accept("Ranjan");
     }
 }
