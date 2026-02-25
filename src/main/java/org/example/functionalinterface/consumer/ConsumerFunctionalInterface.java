e0e854d3f0d877bdb9bb8d69828856d9178038ca
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-02-25 21:54:31 +0530

    Advanced Java: created Consumer Functional Interface

--- src/main/java/org/example/functionalinterface/consumer/ConsumerFunctionalInterface.java
+++ src/main/java/org/example/functionalinterface/consumer/ConsumerFunctionalInterface.java
@@ -1,4 +1,9 @@
 package org.example.functionalinterface.consumer;
 
+import java.util.function.Consumer;
+
 public class ConsumerFunctionalInterface {
+    public static void main(String[] args) {
+        Consumer<String> c = (name) -> System.out.println(name);
+    }
 }
