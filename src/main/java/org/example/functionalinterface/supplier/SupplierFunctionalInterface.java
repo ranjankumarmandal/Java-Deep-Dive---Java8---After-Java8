a1c9427044e2117a8654fe9d3e02d758aa13c277
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-02-26 21:12:44 +0530

    Advanced Java: completed working for Functional Interface: Supplier Functional Interface

--- src/main/java/org/example/functionalinterface/supplier/SupplierFunctionalInterface.java
+++ src/main/java/org/example/functionalinterface/supplier/SupplierFunctionalInterface.java
@@ -5,5 +5,6 @@ import java.util.function.Supplier;
 public class SupplierFunctionalInterface {
     public static void main(String[] args) {
         Supplier<String> s = () -> "Ranjan";
+        System.out.println(s.get());
     }
 }
