d1edec97b56cc90398b0d7758dbb6eadd0790bc6
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-01 21:53:49 +0530

    Advanced Java: completed working for Predicate Functional Interface

--- src/main/java/org/example/functionalinterface/predicate/PredicateFunctionalInterface.java
+++ src/main/java/org/example/functionalinterface/predicate/PredicateFunctionalInterface.java
@@ -1,4 +1,10 @@
 package org.example.functionalinterface.predicate;
 
+import java.util.function.Predicate;
+
 public class PredicateFunctionalInterface {
+    public static void main(String[] args) {
+        Predicate<Integer> p = (n) -> n % 2 == 0;
+        System.out.println(p.test(2));
+    }
 }
