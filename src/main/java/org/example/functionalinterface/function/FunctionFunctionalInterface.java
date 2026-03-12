4ad87aaec808851965a435c7c2866b6a54ea9ead
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-12 12:15:20 +0530

    Advanced Java: FunctionalInterface Type: completed working for Function

--- src/main/java/org/example/functionalinterface/function/FunctionFunctionalInterface.java
+++ src/main/java/org/example/functionalinterface/function/FunctionFunctionalInterface.java
@@ -1,7 +1,10 @@
 package org.example.functionalinterface.function;
 
+import java.util.function.Function;
+
 public class FunctionFunctionalInterface {
     public static void main(String[] args) {
-
+        Function<String, String> f = (name) -> "Hello " + name;
+        System.out.println(f.apply("Ranjan!"));
     }
 }
