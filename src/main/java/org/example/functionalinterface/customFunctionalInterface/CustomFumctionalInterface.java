f21f0039dd45e7efd10894f6bb82e95a79b82040
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-12 12:30:03 +0530

    Advanced Java: completed utilization of custom functional interface

--- src/main/java/org/example/functionalinterface/customFunctionalInterface/CustomFumctionalInterface.java
+++ src/main/java/org/example/functionalinterface/customFunctionalInterface/CustomFumctionalInterface.java
@@ -7,6 +7,7 @@ interface customFuncInterface {
 
 public class CustomFumctionalInterface {
     public static void main(String[] args) {
-
+        customFuncInterface cfi = () -> System.out.println("Hello Ranjan");
+        cfi.print();
     }
 }
