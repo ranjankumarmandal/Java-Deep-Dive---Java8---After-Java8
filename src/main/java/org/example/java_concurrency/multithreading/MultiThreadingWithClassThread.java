56b815db74d2e36a9816ce0607045660da50c5e8
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-16 11:30:38 +0530

    Advanced Java: created 2 threads by extending class Thread

--- src/main/java/org/example/java_concurrency/multithreading/MultiThreadingWithClassThread.java
+++ src/main/java/org/example/java_concurrency/multithreading/MultiThreadingWithClassThread.java
@@ -1,4 +1,17 @@
 package org.example.java_concurrency.multithreading;
 
+class Thread1 extends Thread {
+
+    public void run() {
+        System.out.println("Thread1");
+    }
+}
+
+class Thread2 extends Thread {
+    public void run() {
+        System.out.println("Thread2");
+    }
+}
+
 public class MultiThreadingWithClassThread {
 }
