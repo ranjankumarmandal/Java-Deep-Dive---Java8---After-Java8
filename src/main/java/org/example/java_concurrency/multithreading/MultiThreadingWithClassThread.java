ad386f064a5d21effc18a4d357a86df771db8d29
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-16 11:32:40 +0530

    Advanced Java: utlized both those very basic threads inside method or main thread

--- src/main/java/org/example/java_concurrency/multithreading/MultiThreadingWithClassThread.java
+++ src/main/java/org/example/java_concurrency/multithreading/MultiThreadingWithClassThread.java
@@ -14,4 +14,11 @@ class Thread2 extends Thread {
 }
 
 public class MultiThreadingWithClassThread {
+    public static void main(String[] args) {
+        Thread1 th1 = new Thread1();
+        Thread2 th2 = new Thread2();
+
+        th1.start();
+        th2.start();
+    }
 }
