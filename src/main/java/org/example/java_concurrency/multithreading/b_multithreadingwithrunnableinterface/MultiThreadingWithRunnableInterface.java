f5434a0c580d121171748c9058bbb40436911d3c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-18 21:52:12 +0530

    ADvanced Java: Concurrency: used synchronized keyword to avoid deadlock situation

--- src/main/java/org/example/java_concurrency/multithreading/b_multithreadingwithrunnableinterface/MultiThreadingWithRunnableInterface.java
+++ src/main/java/org/example/java_concurrency/multithreading/b_multithreadingwithrunnableinterface/MultiThreadingWithRunnableInterface.java
@@ -1,19 +1,32 @@
 package org.example.java_concurrency.multithreading.b_multithreadingwithrunnableinterface;
 
-class MyTask implements Runnable {
+class OrderProcessor implements Runnable {
+    private int orderId;
+
+    public OrderProcessor(int orderId) {
+        this.orderId = orderId;
+    }
+
     public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(Thread.currentThread().getName() + " - " + i);
+        System.out.println("Processing Order: " + orderId +
+                " by " + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(2000); // simulate processing time
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        System.out.println("Completed Order: " + orderId);
     }
 }
 
 public class MultiThreadingWithRunnableInterface {
     public static void main(String[] args) {
-        Thread t1 = new Thread(new MyTask());
-        Thread t2 = new Thread(new MyTask());
 
-        t1.start();
-        t2.start();
+        for (int i = 1; i <= 5; i++) {
+            Thread t = new Thread(new OrderProcessor(i));
+            t.start();
+        }
     }
 }
