4d3c218af27f99be0a55fc7fde1a5a603ad514da
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-18 21:43:58 +0530

    Advnced java: concurrency: multi threading with interface Runnable: started the thread t1

--- src/main/java/org/example/java_concurrency/multithreading/b_multithreadingwithrunnableinterface/MultiThreadingWithRunnableInterface.java
+++ src/main/java/org/example/java_concurrency/multithreading/b_multithreadingwithrunnableinterface/MultiThreadingWithRunnableInterface.java
@@ -1,27 +1,15 @@
 package org.example.java_concurrency.multithreading.b_multithreadingwithrunnableinterface;
 
-class Thread1 implements Runnable {
-
-    @Override
+class MyTask implements Runnable {
     public void run() {
-        System.out.println("Thread1");
-    }
-}
-
-class Thread2 implements Runnable {
-
-    @Override
-    public void run() {
-        System.out.println("Thread2");
+        System.out.println("Thread is running");
     }
 }
 
 public class MultiThreadingWithRunnableInterface {
     public static void main(String[] args) {
-        Thread1 t1 = new Thread1();
-        Thread2 t2 = new Thread2();
-
-        t1.run();
-        t2.run();
+        MyTask task = new MyTask();
+        Thread t1 = new Thread(task);
+        t1.start(); // starts a new thread
     }
 }
