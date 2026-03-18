4c702e2e6d5fd43a210b691f9048f475f93acc0f
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-18 21:31:38 +0530

    Advanced Java: concurrency: working for multithreadingwithrunnableinterface: created 2 threads by implementing interface Runnable

--- src/main/java/org/example/java_concurrency/multithreading/b_multithreadingwithrunnableinterface/MultiThreadingWithRunnableInterface.java
+++ src/main/java/org/example/java_concurrency/multithreading/b_multithreadingwithrunnableinterface/MultiThreadingWithRunnableInterface.java
@@ -1,4 +1,20 @@
 package org.example.java_concurrency.multithreading.b_multithreadingwithrunnableinterface;
 
+class Thread1 implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Thread1");
+    }
+}
+
+class Thread2 implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Thread2");
+    }
+}
+
 public class MultiThreadingWithRunnableInterface {
 }
