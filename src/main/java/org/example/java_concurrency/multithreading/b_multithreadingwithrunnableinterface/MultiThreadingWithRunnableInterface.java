0a988a6c5780265d3e0bb14f69d39f5fc79027ac
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-18 21:37:38 +0530

    Advanced Java: concurrency: working for multithreadingwithrunnableinterface: utilized both threads inside main() or main thread

--- src/main/java/org/example/java_concurrency/multithreading/b_multithreadingwithrunnableinterface/MultiThreadingWithRunnableInterface.java
+++ src/main/java/org/example/java_concurrency/multithreading/b_multithreadingwithrunnableinterface/MultiThreadingWithRunnableInterface.java
@@ -17,4 +17,11 @@ class Thread2 implements Runnable {
 }
 
 public class MultiThreadingWithRunnableInterface {
+    public static void main(String[] args) {
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+
+        t1.run();
+        t2.run();
+    }
 }
