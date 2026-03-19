3cc6a0cb9356c661f7ceeae2fb8e43e8331f4bd0
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-19 23:45:00 +0530

    Advanced Java: Concurrency: ExecutorService: started working for it

--- src/main/java/org/example/java_concurrency/multithreading/c_multithreadingwith_executorservice/MultithreadingwithExecutorService.java
+++ src/main/java/org/example/java_concurrency/multithreading/c_multithreadingwith_executorservice/MultithreadingwithExecutorService.java
@@ -1,4 +1,7 @@
 package org.example.java_concurrency.multithreading.c_multithreadingwith_executorservice;
 
 public class MultithreadingwithExecutorService {
+    public static void main(String[] args) {
+        System.out.println("Hello ExecutorService");
+    }
 }
