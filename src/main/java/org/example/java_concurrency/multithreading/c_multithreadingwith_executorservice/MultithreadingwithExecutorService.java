42e0d158ac097bae9ed4f099caf214c34168a39b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-20 22:36:09 +0530

    Advanced Java: Concurrency: Executor Service: executed both threads with es

--- src/main/java/org/example/java_concurrency/multithreading/c_multithreadingwith_executorservice/MultithreadingwithExecutorService.java
+++ src/main/java/org/example/java_concurrency/multithreading/c_multithreadingwith_executorservice/MultithreadingwithExecutorService.java
@@ -9,5 +9,10 @@ public class MultithreadingwithExecutorService {
 
         Runnable t1 = () -> System.out.println("Thread 1");
         Runnable t2 = () -> System.out.println("Thread 2");
+
+        es.submit(t1);
+        es.submit(t2);
+
+        es.shutdown();
     }
 }
